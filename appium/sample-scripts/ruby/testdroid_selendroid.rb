15a726ce54ab9ada69f2123427b1b290e6b56734
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2014-04-16 11:09:39 -0700

    Testdroid - Android Appium example for Ruby using Selendroid

--- appium/sample-scripts/ruby/testdroid_selendroid.rb
+++ appium/sample-scripts/ruby/testdroid_selendroid.rb
@@ -2,7 +2,7 @@
 # install the required gems with bundler by doing:
 #   "bundle install"
 # execute tests using rspec:
-#  rspec testdroid_ios.rb
+#  rspec testdroid_selendroid.rb
 require 'json'
 require 'rspec'
 require 'selenium-webdriver'
@@ -17,8 +17,8 @@ include Selenium
 screen_shot_dir= "screenshot-folder"
 testdroid_username = ENV["TESTDROID_USERNAME"]
 testdroid_password = ENV["TESTDROID_PASSWORD"]
-testdroid_device = "iPad 2 A1395 5.1" # Example device. Change if you desire.
-testdroid_app_file = "BitbarIOSSample.ipa" 
+testdroid_device = "Samsung Galaxy Epic 4G SPH-D700" # Example device. Change if you desire.
+testdroid_app_file = "BitbarAndroidSample.apk" 
 
 
 def log(msg)
@@ -26,15 +26,16 @@ def log(msg)
 end
 @testdroid_app=nil
 desired_capabilities_cloud={
-        'app'=> 'com.bitbar.testdroid.BitbarIOSSample',
-        'device'=> 'iphone',
+        'device'=> 'Selendroid',
         'testdroid_app'=> nil,
         'testdroid_username'=> testdroid_username,
         'testdroid_password'=> testdroid_password,
-        'testdroid_project'=> 'Appium iOS Project1',
+        'testdroid_project'=> 'Appium Android(Selendroid) demo',
         'testdroid_description'=> 'Appium project description',
-        'testdroid_testrun'=> 'Test Run 1',
+        'testdroid_testrun'=> 'Selendroid Run 1',
         'testdroid_device'=> testdroid_device,
+        'app-package' => 'com.bitbar.testdroid',
+        'app-activity' => '.BitbarSampleApplicationActivity',
     }
 
 
@@ -48,13 +49,13 @@ def upload_application(file_path, username, password)
   c.password = password
   c.multipart_form_post = true
   c.verbose = true
-  c.http_post(Curl::PostField.file('BitbarIOSSample.ipa', file_path))
+  c.http_post(Curl::PostField.file('BitbarAndroidSample.apk', file_path))
   resp = JSON.parse(c.body_str)
 
-  @testdroid_app = resp["value"]["uploads"]["BitbarIOSSample.ipa"]
+  @testdroid_app = resp["value"]["uploads"]["BitbarAndroidSample.apk"]
 end
 
-describe "BitbarIOSSample testing" do
+describe "BitbarAndroidSample testing" do
   before :all do
  
     log ("Upload application #{testdroid_app_file}")
@@ -68,8 +69,12 @@ describe "BitbarIOSSample testing" do
     @driver = Selenium::WebDriver.for(:remote, :desired_capabilities => desired_capabilities_cloud, :url => server_url, :http_client => http_client)
 
     log ("WebDriver response received")
-   end
-
+  end
+  after(:each) do
+  	if example.exception != nil
+    	@driver.save_screenshot(screen_shot_dir + "#{example.description.gsub(' ', '_')}") unless @driver.nil?
+  		end
+	end
   after :all do
     log ("Stop WebDriver")
 
@@ -77,64 +82,47 @@ describe "BitbarIOSSample testing" do
   end
 
   it "should show failure page"  do
-    log ("view1: Finding buttons")
-    buttons = @driver.find_elements(:tag_name, :button)
-    log ("view1: Clicking button [0] - RadioButton 1")
-    buttons[0].click()
-
-    log ("view1: Typing in textfield[0]: Testdroid user")
-    @driver.find_elements(:tag_name,  :textField)[0].send_keys("Testdroid user")
-
-    y = 0.95
-    x = 0.5
-    log ("view1: Tapping at position (384, 0.95) - Estimated position of SpaceBar")
-    @driver.execute_script("mobile: tap",{"touchCount" => "1","x" => x,"y" => y})
-
+    log ("view1: Clicking RadioButton 0 - 'Buy 101 devices'")
+    @driver.find_elements(:id, 'radio0')[0].click()
+    
+    log ("view1: Typing in EditText[0]: Testdroid user")
+    @driver.find_elements(:tag_name,  :EditText)[0].send_keys("Testdroid user")
+    
     log ("view1: Taking screenshot screenshot1.png")
     @driver.save_screenshot(screen_shot_dir + "/screenshot1.png")
-    sleep(5)
-    log ("view1: Hiding Keyboard")
-    @driver.execute_script('UIATarget.localTarget().frontMostApp().keyboard().buttons()["Hide keyboard"].tap();')
+    sleep(2)
+    @driver.navigate.back()
+  
+    log ("view1: Clicking button Answer")
+    @driver.find_elements(:tag_name, :Button)[0].click()
 
-    log ("view1: Taking screenshot screenshot2.png")
+    log ("view2: Taking screenshot screenshot2.png")
     @driver.save_screenshot(screen_shot_dir + "/screenshot2.png")
-    sleep(5)
-    log ("view1: Clicking button[6] - OK  Button")
-    buttons[6].click()
-
-    log ("view2: Taking screenshot screenshot3.png")
-    @driver.save_screenshot(screen_shot_dir + "/screenshot3.png")
-    sleep(5)
+    find_elements(:link_text, 'Wrong Answer!').size == 1
+    sleep(2)
   end
 
   it "should click back button" do
      # view2
-    log ("view2: Finding buttons")
-    buttons = @driver.find_elements(:tag_name, :button)
-    log ("view2: Clicking button[0] - Back/OK button")
-    buttons[0].click()
+    log ("view2: Go back")
+    @driver.navigate.back()
   end
 
   it "should click 2nd radio button" do
-    log ("view1: Finding buttons")
-    buttons = @driver.find_elements(:tag_name, :button)
-    log ("view1: Clicking button[2] - RadioButton 2")
-    buttons[2].click()
+    log ("view1: Clicking RadioButton 1 - 'Use Testdroid Cloud'")
+    @driver.find_elements(:id, 'radio1')[0].click()
+    @driver.save_screenshot(screen_shot_dir + "/screenshot3.png")
+    log ("view1: Clicking Answer")
+    @driver.find_elements(:tag_name, :Button)[0].click()
 
-    log ("view1: Clicking button[6] - OK Button")
-    buttons[6].click()
+
     log ("view1: Taking screenshot screenshot4.png")
     @driver.save_screenshot(screen_shot_dir + "/screenshot4.png")
-
+    @driver.find_elements(:link_text, 'You are right!').size == 1
+    
     log ("view1: Sleeping 3 before quitting webdriver")
     sleep(3)
 
-  end
-
-  it "should get window size" do
-    size = @driver.manage.window.size
-    size.width.should eq(768)
-    size.height.should eq(1024)
   end
 
 end
