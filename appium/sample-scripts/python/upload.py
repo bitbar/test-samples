1adfaec371d50ffc4cae071ff65789b05e0ba0d7
Jarno Tuovinen <jarno.tuovinen@bitbar.com>
2015-01-15 13:54:12 +0200

    Add example how to upload app for Appium testing

---
+++ appium/sample-scripts/python/upload.py
@@ -0,0 +1,19 @@
+#
+# Example how to upload application to Testdroid Cloud before Appium test
+#
+
+import requests
+import base64
+
+username = 'USERNAME'
+password = 'PASSWORD'
+upload_url = 'http://appium.testdroid.com/upload'
+myfile = '../../../apps/builds/Testdroid.apk'
+
+def build_headers():
+  return { 'Authorization' : 'Basic %s' % base64.b64encode(username+":"+password) }
+
+files = {'file': open(myfile, 'rb')}
+r = requests.post(upload_url, files=files, headers=build_headers())
+print r.text
+
