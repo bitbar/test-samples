bfe61b022f13a49bf15c56f48b82726fcb2bc76b
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2015-09-01 12:38:58 -0700

    Release appium session in any case

--- appium/sample-scripts/python/testdroid_ios.py
+++ appium/sample-scripts/python/testdroid_ios.py
@@ -58,61 +58,63 @@ desired_capabilities_cloud={
 # set up webdriver
 log ("WebDriver request initiated. Waiting for response, this typically takes 2-3 mins")
 
-driver = webdriver.Remote(command_executor=appium_url,desired_capabilities=desired_capabilities_cloud)
-log ("WebDriver response received")
+try:
+    driver = webdriver.Remote(command_executor=appium_url,desired_capabilities=desired_capabilities_cloud)
+    log ("WebDriver response received")
 
-# view1
-log ("view1: Finding buttons")
-buttons = driver.find_elements_by_class_name('UIAButton')
-log ("view1: Clicking button [0] - RadioButton 1")
-buttons[0].click()
+    # view1
+    log ("view1: Finding buttons")
+    buttons = driver.find_elements_by_class_name('UIAButton')
+    log ("view1: Clicking button [0] - RadioButton 1")
+    buttons[0].click()
 
-log ("view1: Typing in textfield[0]: Testdroid user")
-elem = driver.find_element_by_class_name('UIATextField')
-elem.clear()
-elem.send_keys('Testdroid user')
+    log ("view1: Typing in textfield[0]: Testdroid user")
+    elem = driver.find_element_by_class_name('UIATextField')
+    elem.clear()
+    elem.send_keys('Testdroid user')
 
-y = 0.95
-log ("view1: Tapping at position (384, 0.95) - Estimated position of SpaceBar")
-driver.execute_script("mobile: tap",{"touchCount":"1","x":"0.5","y":y})
+    y = 0.95
+    log ("view1: Tapping at position (384, 0.95) - Estimated position of SpaceBar")
+    driver.execute_script("mobile: tap",{"touchCount":"1","x":"0.5","y":y})
 
-log ("view1: Taking screenshot screenshot1.png")
-driver.save_screenshot(screenshot_dir + "/screenshot1.png")
-#driver.execute_script('UIATarget.localTarget().captureScreenWithName("screenshot1");') # takes screenshot at server-side
+    log ("view1: Taking screenshot screenshot1.png")
+    driver.save_screenshot(screenshot_dir + "/screenshot1.png")
+    #driver.execute_script('UIATarget.localTarget().captureScreenWithName("screenshot1");') # takes screenshot at server-side
 
-log ("view1: Hiding Keyboard")
-driver.find_element_by_name("Return").click()
-#driver.execute_script('UIATarget.localTarget().frontMostApp().keyboard().buttons()["Hide keyboard"].tap();')
+    log ("view1: Hiding Keyboard")
+    driver.find_element_by_name("Return").click()
+    #driver.execute_script('UIATarget.localTarget().frontMostApp().keyboard().buttons()["Hide keyboard"].tap();')
 
-log ("view1: Taking screenshot screenshot2.png")
-driver.save_screenshot(screenshot_dir + "/screenshot2.png")
+    log ("view1: Taking screenshot screenshot2.png")
+    driver.save_screenshot(screenshot_dir + "/screenshot2.png")
 
-log ("view1: Clicking button[6] - OK  Button")
-buttons[6].click()
+    log ("view1: Clicking button[6] - OK  Button")
+    buttons[6].click()
 
-log ("view2: Taking screenshot screenshot3.png")
-driver.save_screenshot(screenshot_dir + "/screenshot3.png")
+    log ("view2: Taking screenshot screenshot3.png")
+    driver.save_screenshot(screenshot_dir + "/screenshot3.png")
 
-# view2
-log ("view2: Finding buttons")
-buttons = driver.find_elements_by_class_name('UIAButton')
-log ("view2: Clicking button[0] - Back/OK button")
-buttons[0].click()
+    # view2
+    log ("view2: Finding buttons")
+    buttons = driver.find_elements_by_class_name('UIAButton')
+    log ("view2: Clicking button[0] - Back/OK button")
+    buttons[0].click()
 
-# view 1
-log ("view1: Finding buttons")
-buttons = driver.find_elements_by_class_name('UIAButton')
-log ("view1: Clicking button[2] - RadioButton 2")
-buttons[2].click()
+    # view 1
+    log ("view1: Finding buttons")
+    buttons = driver.find_elements_by_class_name('UIAButton')
+    log ("view1: Clicking button[2] - RadioButton 2")
+    buttons[2].click()
 
-log ("view1: Clicking button[6] - OK Button")
-buttons[6].click()
+    log ("view1: Clicking button[6] - OK Button")
+    buttons[6].click()
 
-log ("view1: Taking screenshot screenshot4.png")
-driver.save_screenshot(screenshot_dir + "/screenshot4.png")
+    log ("view1: Taking screenshot screenshot4.png")
+    driver.save_screenshot(screenshot_dir + "/screenshot4.png")
 
-log ("view1: Sleeping 3 before quitting webdriver")
-sleep(3)
+    log ("view1: Sleeping 3 before quitting webdriver")
+    sleep(3)
 
-log ("view1: Quitting WebDriver")
-driver.quit()
+finally:
+    log ("view1: Quitting WebDriver")
+    driver.quit()
