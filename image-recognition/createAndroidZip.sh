6afaffed3d89c846268bb68294b96ced623217a1
Jarno Tuovinen <jarno.tuovinen@bitbar.com>
2016-05-31 09:24:10 +0300

    Improve zip creator scripts

--- image-recognition/createAndroidZip.sh
+++ image-recognition/createAndroidZip.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+#
+# Create zip ready to be uploaded to Testdroid Cloud.
+#
+
+OUT_ZIP="android-test.zip"
 
 cp run-tests_android.sh run-tests.sh
-rm android-test.zip
-zip -r android-test.zip pom.xml run-tests.sh src akaze/linux akaze/LICENSE lib/testdroid-appium-driver-1.2.0.jar queryimages
+if [ -f "${OUT_ZIP}" ]; then
+    rm "${OUT_ZIP}"
+fi
+zip -r "${OUT_ZIP}" pom.xml run-tests.sh src akaze/linux akaze/LICENSE lib/testdroid-appium-driver-1.2.0.jar queryimages
