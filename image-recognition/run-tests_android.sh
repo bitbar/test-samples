2b4c49e02fa3e394d36edc74b92f0fe92d94ecc2
LasseHall <lasse.hall@bitbar.com>
2016-10-14 18:47:56 +0300

    run-tests_android.sh and run-tests_ios.sh restructuring

--- image-recognition/run-tests_android.sh
+++ image-recognition/run-tests_android.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-## This is the Android version of run-tests.sh for Server side execution
-## To use in cloud:
-## 		Replace the TEST and JUNIT variables with your desired test suite
-##    execute ./createAndroidZip.sh to create the test zip for upload in cloud
-
-# Name of the desired test suite and optionally specific test case, eg: iOSSample#mainPageTest
-TEST=${TEST:="AndroidSample#mainPageTest"}
-# JUnit file wont have the #caseName ending
-JUNIT="AndroidSample"
-
-export SCREENSHOT_FOLDER=target/reports/screenshots/android/
-export PLATFORM_NAME=Android
-export UDID=${ANDROID_SERIAL}
-#export LD_LIBRARY_PATH=/opt/OpenCV/opencv-2.4.9/build/lib
-
-echo "Starting Appium ..."
-appium-1.4 --command-timeout 120 --log-no-colors --log-timestamp >appium.log 2>&1 &
-
-ps -ef|grep appium
-
+# Important! This step is needed to make sure the test zip files are extracted
 echo "Extracting tests.zip..."
 unzip tests.zip
 
-mvn --quiet install:install-file -Dfile=lib/linux/opencv/java7/opencv-2413.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.13 -Dpackaging=jar
+## This is the Android version of run-tests.sh to be used as template for Server side execution
+## To use in cloud:
+##  Replace the TEST and TEST_CASE variables with your desired test class and cases
+##  execute ./createAndroidZip.sh to create the test zip for upload in cloud
 
+# Name of the desired test class and optionally specific test case, eg: AndroidSample#mainPageTest
+TEST=${TEST:="AndroidSample"}
+# OPTIONAL: add the name of TestCases to be used with the `mvn test` command
+# Leave blank to test the whole class!
+TEST_CASE="#mainPageTest"
+
+## Environment variables setup
+export SCREENSHOT_FOLDER=target/reports/screenshots/android/
+export PLATFORM_NAME=Android
+export UDID=${ANDROID_SERIAL}
+
+# Check Android device API level to set Appium automationName
 APILEVEL=$(adb shell getprop ro.build.version.sdk)
 APILEVEL="${APILEVEL//[$'\t\r\n']}"
+export PLATFORM_VERSION=${APILEVEL}
 echo "API level is: ${APILEVEL}"
 if [ "$APILEVEL" -gt "16" ]; then
 	export AUTOMATION_NAME=appium
@@ -34,16 +31,32 @@ else
 	export AUTOMATION_NAME=selendroid
 fi
 
+## Appium server launch
+# Use appium-1.4 to target 1.4.x and appium-1.5 to target 1.5.x
+echo "Starting Appium ..."
+appium-1.4 --command-timeout 120 --log-no-colors --log-timestamp >appium.log 2>&1 &
+
+# Check that the appium process exists
+ps -ef|grep appium
+
+## Dependency installation
+# Install the OpenCV java bindings
+mvn --quiet install:install-file -Dfile=lib/linux/opencv/java7/opencv-2413.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.13 -Dpackaging=jar
+
+# Make sure the akaze binaries have execution rights
 chmod +x lib/*
 
 ## Start test execution
-echo "Running test ${TEST}"
-mvn -Dtest=${TEST} --quiet clean test
+echo "Running tests ${TEST}${TEST_CASE}"
+# Remove `-Dtest=${TEST}${TEST_CASE}` to launch all tests in the project
+# More examples at https://maven.apache.org/surefire/maven-surefire-plugin/examples/single-test.html
+mvn -Dtest=${TEST}${TEST_CASE} --quiet clean test
 
 ## Post-processing
-# JUnit results need to be at root as "TEST-all.xml"
-#mv target/reports/junit/TEST-${JUNIT}.xml TEST-all.xml
-ln -s target/reports/junit/TEST-${JUNIT}.xml TEST-all.xml
-# Screenshots need to be at screenshots directory in root.
-rm -r screenshots
-ln -s target/reports/screenshots/android screenshots
+# JUnit results need to be available at root as "TEST-all.xml"
+ln -s target/reports/junit/TEST-${TEST}.xml TEST-all.xml
+
+# Make sure there's no pre-existing `screenshots` file blocking symbolic link creation
+rm -rf screenshots
+# Screenshots need to be available at root as directory `screenshots` .
+ln -s ${SCREENSHOT_FOLDER} screenshots
--- image-recognition/run-tests_ios.sh
+++ image-recognition/run-tests_ios.sh
@@ -1,52 +1,64 @@
 #!/bin/bash
 
-## This is the IOS version of run-tests.sh for Server side execution
+# Important! This step is needed to make sure the test zip files are extracted
+echo "Extracting tests.zip..."
+unzip tests.zip
+
+## This is the IOS version of run-tests.sh to be used as template for Server side execution
 ## To use in cloud:
-## 		Replace the TEST and JUNIT variables with your desired test suite
-##    execute ./createiOSZip.sh to create the test zip for upload in cloud
+##  Replace the TEST and TEST_CASE variables with your desired test class and cases
+##  execute ./createiOSZip.sh to create the test zip for upload in cloud
 
 # Name of the desired test suite and optionally specific test case, eg: iOSSample#mainPageTest
-TEST=${TEST:="iOSSample#mainPageTest"}
-# JUnit file wont have the #caseName ending
-JUNIT="iOSSample"
+TEST=${TEST:="iOSSample"}
+# OPTIONAL: add the name of TestCases to be used with the `mvn test` command
+# Leave blank to test the whole class!
+TEST_CASE="#mainPageTest"
 
+## Environment variables setup
 export SCREENSHOT_FOLDER=target/reports/screenshots/ios/
 export PLATFORM_NAME=iOS
 export JAVA_HOME=$(/usr/libexec/java_home)
+
 export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:$PWD/lib/mac/opencv:/Users/Shared/libimobiledevice-binaries-master
-#export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/Users/Shared/libimobiledevice-binaries-master
 # Replace all spaces with escape: "\ "
 export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH// /\\ }
 echo "DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"
 
-## Cloud setup
+export PLATFORM_VERSION=$(xcrun --sdk iphonesimulator --show-sdk-version)
+echo "iOS Version: ${PLATFORM_VERSION}"
+
+export UDID=$IOS_UDID
 echo "UDID is ${UDID}"
+
+## Appium server launch
+# Use appium-1.4 to target 1.4.x and appium-1.5 to target 1.5.x
 echo "Starting Appium ..."
 appium-1.5 -U ${UDID} --command-timeout 120 --log-no-colors --log-timestamp >appium.log 2>&1 &
+# Launch ios-webkit-debug-proxy-launcher.js for webview handling in iOS
 /opt/testdroid/appium-1.4.16-testdroid/bin/ios-webkit-debug-proxy-launcher.js -c ${UDID}:27753 -d >ios-webkit-debug-proxy.log 2>&1 &
 
+# Check that the appium process exists
 ps -ef|grep appium
 
-echo "Extracting tests.zip..."
-unzip tests.zip
-
+## Dependency installation
+# Install the OpenCV java bindings
 mvn --quiet install:install-file -Dfile=lib/mac/opencv/opencv-2413.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.13 -Dpackaging=jar
-mkdir -p screenshots
 
-ideviceinstaller -l
-
-echo "xcrun --sdk iphonesimulator --show-sdk-version"
-xcrun --sdk iphonesimulator --show-sdk-version
-
+# Make sure the akaze binaries have execution rights
 chmod +x lib/*
 
 ## Start test execution
-echo "Running test ${TEST}"
-mvn -Dtest=${TEST} --quiet clean test
+echo "Running tests ${TEST}${TEST_CASE}"
+# Remove `-Dtest=${TEST}${TEST_CASE}` to launch all tests in the project
+# More examples at https://maven.apache.org/surefire/maven-surefire-plugin/examples/single-test.html
+mvn -Dtest=${TEST}${TEST_CASE} --quiet clean test
 
 ## Post-processing
-# JUnit results need to be at root as "TEST-all.xml"
-ln -s target/reports/junit/TEST-${JUNIT}.xml TEST-all.xml
-# Screenshots need to be at screenshots directory in root.
-rm -r screenshots
-ln -s target/reports/screenshots/ios screenshots
+# JUnit results need to be available at root as "TEST-all.xml"
+ln -s target/reports/junit/TEST-${TEST}.xml TEST-all.xml
+
+# Make sure there's no pre-existing `screenshots` file blocking symbolic link creation
+rm -rf screenshots
+# Screenshots need to be available at root as directory `screenshots` .
+ln -s ${SCREENSHOT_FOLDER} screenshots
