61757dfb062a6cce5bacf3e673d9de52a03c0d99
Niko Cankar <niko.cankar@gmail.com>
2017-01-10 17:23:06 +0200

    python server side sample

---
+++ appium/server-side/python/run-tests_android.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+##
+## Work in progress! The dependency installations need to be done to the
+## container so that we don't need to install them here.
+##
+TEST=${TEST:="BitbarSampleAppTest.py"} #Name of the test file
+
+##### Cloud testrun dependencies start
+echo "Extracting tests.zip..."
+unzip tests.zip
+
+echo "Installing pip for python"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+
+echo "Installing Appium Python Client 0.24 and xmlrunner 1.7.7"
+chmod 0755 requirements.txt
+sudo pip install -r requirements.txt
+
+echo "Starting Appium ..."
+
+/opt/appium/appium/bin/appium.js >appium.log 2>&1 &
+
+sleep 10 # Sleep for appium to launch properly
+
+ps -ef|grep appium
+##### Cloud testrun dependencies end.
+
+export APPIUM_APPFILE=$PWD/application.apk #App file is at current working folder
+
+## Desired capabilities:
+
+export APPIUM_URL="http://localhost:4723/wd/hub" # Local & Cloud
+export APPIUM_DEVICE="Local Device"
+export APPIUM_PLATFORM="android"
+
+APILEVEL=$(adb shell getprop ro.build.version.sdk)
+APILEVEL="${APILEVEL//[$'\t\r\n']}"
+echo "API level is: ${APILEVEL}"
+
+## APPIUM_AUTOMATION 
+if [ "$APILEVEL" -gt "16" ]; then
+  echo "Setting APPIUM_AUTOMATION=android"
+  export APPIUM_AUTOMATION="Android"
+else
+  echo "Setting APPIUM_AUTOMATION=selendroid"
+  export APPIUM_AUTOMATION="Selendroid"
+fi
+
+## Run the test:
+echo "Running test ${TEST}"
+rm -rf screenshots
+
+python ${TEST}
+
+mv test-reports/*.xml TEST-all.xml
