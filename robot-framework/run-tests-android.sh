b5fd793cccfc6c2001c889dd7e6f48c9e11076f7
Niko Cankar <niko.cankar@gmail.com>
2017-12-19 14:55:15 +0200

    moving things around

--- robot-framework/run-tests-android.sh
+++ robot-framework/run-tests-android.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-## Cloud setup
-echo "Starting Appium ..."
-
-appium-1.6 --log-no-colors --log-timestamp --command-timeout 120
 
 echo "Extracting tests.zip..."
-unzip tests.zip
+unzip -o tests.zip
 
 echo "Installing pip"
 curl -O https://bootstrap.pypa.io/get-pip.py
@@ -14,6 +10,10 @@ python get-pip.py --user
 
 echo "Installing requirements"
 ~/.local/bin/pip install -r ./resources/requirements.txt --target .
+
+## start Appium server
+echo "Starting Appium ..."
+appium-1.6 --log-no-colors --log-timestamp --command-timeout 120
 
 ## Start test execution
 echo "Running test"
