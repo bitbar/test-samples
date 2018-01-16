d18adaef465e2b7633ec9627dc9ca46d1186b52c
Niko Cankar <niko.cankar@gmail.com>
2018-01-16 17:02:41 +0200

    updated to use the sample page

--- appium/sample-scripts/javascript/nightwatch_web_example/tests/example_test.js
+++ appium/sample-scripts/javascript/nightwatch_web_example/tests/example_test.js
@@ -2,10 +2,10 @@
 module.exports = {
     'Basic Test Example' : function (browser) {
         browser
-            .url('http://testdroid.com/')
+            .url('https://bitbar.github.io/testdroid-samples/')
             .waitForElementVisible('body', 4000)
             .pause(2000)
-            .assert.title('Mobile App Testing | Testdroid Technology by Bitbar')
+            .assert.title("Bitbar - Test Page for Samples")
             .end();
     }
 };
