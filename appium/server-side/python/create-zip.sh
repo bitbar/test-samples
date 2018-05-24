e152fc61285c4dd77e4c051724f6f170645a9bc4
Niko Cankar <niko.cankar@gmail.com>
2018-05-24 15:47:59 +0300

    script to zip necessary files

---
+++ appium/server-side/python/create-zip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# Copyright(C) 2018 Bitbar Technologies Oy
+#
+# NOTE: contributions are welcome
+#
+# __author__ = 'Niko Cankar <niko.cankar@bitbar.com>'
+
+
+# Print how script is used and exit
+function print_help_and_die() {
+    echo "Usage: $0 <ios|android>"
+    exit 0
+}
+
+if [[ "$#" -eq 1 ]]; then
+    ENV="$1"
+else
+    print_help_and_die
+fi
+
+TEST_FILE="test-package_${ENV}.zip"
+
+echo "Creating test file for environment: ${ENV}"
+cp run-tests_${ENV}.sh run-tests.sh && zip -r "${TEST_FILE}" requirements.txt *py run-tests.sh
+echo "
+You should now upload test file '${TEST_FILE}' to Bitbar Cloud"
