afa81d822a2bc310949c353dc3e8b99d47ef0ccf
Niko Cankar <niko.cankar@gmail.com>
2018-06-13 17:25:51 +0300

    ios appium started with appium wrapper

--- appium/server-side/python/BitbarSampleAppTest.py
+++ appium/server-side/python/BitbarSampleAppTest.py
@@ -15,12 +15,12 @@ class BitbarSampleAppTest(TestdroidAppiumTest):
     # Test start.    
     def test_the_app(self):
         driver = self.get_driver() # Initialize Appium connection to device
-        
+
         sleep(10) # Wait that the app loads
         log("Start!")
         # Use this to get detected screen hierarchy
         # print self.driver.page_source
-        
+
         if self.isAndroid():
             #try:
                 log("Taking screenshot 0_appLaunch.png")
@@ -56,7 +56,32 @@ class BitbarSampleAppTest(TestdroidAppiumTest):
                 sleep(3)
             except WebDriverException:
                 log("Testrun failed..")
+        driver.quit()
                 
+    def test_the_app_second(self):
+        driver = self.get_driver() # Initialize Appium connection to device
+
+        sleep(10) # Wait that the app loads
+        log("Start!")
+        # Use this to get detected screen hierarchy
+        # print self.driver.page_source
+
+    #try:
+        log("Taking screenshot 0_appLaunch.png")
+        driver.save_screenshot(self.screenshot_dir + "/0_appLaunch.png")
+        log("Clicking element 'Use Testdroid Cloud'")
+        if self.isSelendroid():
+                elem = driver.find_element_by_xpath("//LinearLayout[1]/FrameLayout[1]/ScrollView[1]/LinearLayout[1]/LinearLayout[1]/RadioGroup[1]/RadioButton[2]")
+        else:
+            elem = self.driver.find_element_by_android_uiautomator('new UiSelector().text("Use Testdroid Cloud")')
+        self.assertTrue(elem)
+        elem.click()
+        sleep(2) # always sleep before taking screenshot to let transition animations finish
+        log("Taking screenshot: 1_radiobuttonPressed.png")
+        driver.save_screenshot(self.screenshot_dir + "/1_radiobuttonPressed.png")
+        log("Sleeping 3 before quitting webdriver")
+        sleep(3)
+
     # Test end.
 
 if __name__ == '__main__':
