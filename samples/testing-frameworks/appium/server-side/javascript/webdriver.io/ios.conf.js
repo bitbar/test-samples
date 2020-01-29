d12a2840db584260d804ac5a1be89cd565b310d2
Łukasz Siudakiewicz <lukasz.siudakiewicz@bitbar.com>
2020-01-29 11:25:28 +0100

    Adding iOS Cloud Side Appium JavaScript example

---
+++ samples/testing-frameworks/appium/server-side/javascript/webdriver.io/ios.conf.js
@@ -0,0 +1,204 @@
+const path = require('path');
+
+exports.config = {
+    runner: 'local',
+
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+    logLevel: 'silent',
+    deprecationWarnings: true,
+
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+
+    reporters: [
+        'spec',
+        [
+            'junit', {
+            outputDir: './',
+            outputFileFormat: () => {
+                return 'TEST-all.xml';
+            }
+        }
+        ]
+    ],
+
+    host: '127.0.0.1',
+    port: 4723,
+    path:  '/wd/hub',
+
+    services: ['appium'],
+    appium: {
+        command: 'appium',
+        logPath : './',
+    },
+
+    specs: [
+        './test/specs/ios.js'
+    ],
+
+    capabilities: [{
+        udid: process.env.IOS_UDID,
+        platformName: 'IOS',
+        maxInstances: 1,
+
+        'appium:deviceName': 'Local Device',
+        'appium:automationName': 'XCUITest',
+        'appium:app': path.resolve('application.ipa'),
+        'appium:newCommandTimeout': 240
+    }],
+
+    //
+    // =====
+    // Hooks
+    // =====
+    // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
+    // it and to build services around it. You can either apply a single function or an array of
+    // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
+    // resolved to continue.
+
+    /**
+     * Gets executed once before all workers get launched.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     */
+    // onPrepare: function (config, capabilities) {
+    // },
+
+    /**
+     * Gets executed just before initialising the webdriver session and test framework. It allows you
+     * to manipulate configurations depending on the capability or spec.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     */
+    // beforeSession: function (config, capabilities, specs) {
+    // },
+
+    /**
+     * Gets executed before test execution begins. At this point you can access to all global
+     * variables like `browser`. It is the perfect place to define custom commands.
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     */
+    // before: function (capabilities, specs) {
+    // },
+    before: function() {
+        const chai = require('chai');
+        global.expect = chai.expect;
+        chai.should();
+
+        const fs = require('fs');
+
+        global.takeScreenshot = async (fileName) => {
+            let screenshot = await driver.takeScreenshot();
+            screenshot = screenshot.replace(/^data:image\/png;base64,/, "")
+            let filePath = path.resolve(`./screenshots/${fileName}.png`);
+            fs.writeFileSync(filePath, screenshot, 'base64');
+        };
+
+    }
+
+    /**
+     * Runs before a WebdriverIO command gets executed.
+     * @param {String} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     */
+    // beforeCommand: function (commandName, args) {
+    // },
+
+    /**
+     * Hook that gets executed before the suite starts
+     * @param {Object} suite suite details
+     */
+    // beforeSuite: function (suite) {
+    // },
+
+    /**
+     * Function to be executed before a test (in Mocha/Jasmine) starts.
+     */
+    // beforeTest: function (test, context) {
+    // },
+
+    /**
+     * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
+     * beforeEach in Mocha)
+     */
+    // beforeHook: function (test, context) {
+    // },
+
+    /**
+     * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
+     * afterEach in Mocha)
+     */
+    // afterHook: function (test, context, { error, result, duration, passed, retries }) {
+    // },
+
+    /**
+     * Function to be executed after a test (in Mocha/Jasmine).
+     */
+    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
+    // },
+
+
+    /**
+     * Hook that gets executed after the suite has ended
+     * @param {Object} suite suite details
+     */
+    // afterSuite: function (suite) {
+    // },
+
+    /**
+     * Runs after a WebdriverIO command gets executed
+     * @param {String} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     * @param {Number} result 0 - command success, 1 - command error
+     * @param {Object} error error object if any
+     */
+    // afterCommand: function (commandName, args, result, error) {
+    // },
+
+    /**
+     * Gets executed after all tests are done. You still have access to all global variables from
+     * the test.
+     * @param {Number} result 0 - test pass, 1 - test fail
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
+    // after: function (result, capabilities, specs) {
+    // },
+
+    /**
+     * Gets executed right after terminating the webdriver session.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
+    // afterSession: function (config, capabilities, specs) {
+    // },
+
+    /**
+     * Gets executed after all workers got shut down and the process is about to exit. An error
+     * thrown in the onComplete hook will result in the test run failing.
+     * @param {Object} exitCode 0 - success, 1 - fail
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {<Object>} results object containing test results
+     */
+    // onComplete: function(exitCode, config, capabilities, results) {
+    // },
+
+    /**
+     * Gets executed when a refresh happens.
+     * @param {String} oldSessionId session ID of the old session
+     * @param {String} newSessionId session ID of the new session
+     */
+    //onReload: function(oldSessionId, newSessionId) {
+    //}
+}
---
+++ samples/testing-frameworks/appium/server-side/javascript/webdriver.io/test/specs/android.js
@@ -0,0 +1,35 @@
+describe('Bitbar Sample App', () => {
+    it('Should radio button should be visible ', async () => {
+        let el = await $('//android.widget.RadioButton[@text="Buy 101 devices"]');
+        let visible = await el.isDisplayed();
+        visible.should.be.true;
+    });
+    
+    it('Should show failure page', async () => {
+        let el;
+
+        console.log("view1: Clicking button - 'Buy 101 devices'");
+        el = await $('//android.widget.RadioButton[@text="Buy 101 devices"]');
+        el.click();
+    
+        console.log("view1: Typing in textfield[0]: Bitbar user");
+        el = await $('//android.widget.EditText[@resource-id="com.bitbar.testdroid:id/editText1"]');
+        el.setValue('Bitbar user');
+    
+        driver.hideKeyboard();
+        console.log("view1: Taking screenshot screenshot1.png");
+        await takeScreenshot('screenshot1');
+    
+        console.log("view1: Clicking button Answer");
+        el = await $('//android.widget.Button[@text="Answer"]');
+        el.click();
+    
+        console.log("view2: Taking screenshot screenshot2.png");
+        await takeScreenshot('screenshot2');
+    
+        el = await $('//android.widget.TextView[@text="Wrong Answer!"]');
+        let txt = await el.getText();
+        txt.should.be.equal('Wrong Answer!');
+    });
+
+});
