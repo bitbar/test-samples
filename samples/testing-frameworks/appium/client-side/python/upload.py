c47260f4ae17abe8cc59c954f7decbe3310bde83
Hubert Rudzik <hubert.rudzik@SB-PL-781MD6M.local>
2022-07-11 09:49:48 +0200

    User authorization

--- samples/testing-frameworks/appium/client-side/python/upload.py
+++ samples/testing-frameworks/appium/client-side/python/upload.py
@@ -7,7 +7,9 @@ import base64
 import os
 import requests
 import sys
-
+import urllib
+from requests.auth import HTTPBasicAuth
+import getpass
 
 class UploadApp():
     def __init__(self):
@@ -38,14 +40,17 @@ class UploadApp():
             sys.exit(1)
 
     def build_headers(self):
-        hdrs = {'Authorization': 'Basic %s' % base64.b64encode(self.api_key + ":"),
+        api_key =  base64.b64encode(bytes(self.api_key + ":",  encoding='utf8'))
+        hdrs = {'Authorization': 'Basic %s' % api_key,
                 'Accept': 'application/json'}
         return hdrs
 
     def upload_app(self):
         self.parse_args()
         files = {'file': (os.path.basename(self.myfile), open(self.myfile, 'rb'), 'application/octet-stream')}
-        r = requests.post(self.upload_url, files=files, headers=self.build_headers())
+        password = getpass.getpass("Enter host password for user '" + self.api_key +  "':")
+        r = requests.post(self.upload_url, files=files, headers=self.build_headers(),
+            auth=HTTPBasicAuth(self.api_key, password))
 
         try:
             print("File id to use in bitbar capabilities in your test: {}".format(r.json()['id']))
