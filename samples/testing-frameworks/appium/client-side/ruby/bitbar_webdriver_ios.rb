21c4165d1d00fb8a82608eca1baf9a0b3eea0349
MaciejLendzion-smartbear <108719957+MaciejLendzion-smartbear@users.noreply.github.com>
2022-07-14 14:49:17 +0200

    Webdriver ios update

--- samples/testing-frameworks/appium/client-side/ruby/bitbar_webdriver_ios.rb
+++ samples/testing-frameworks/appium/client-side/ruby/bitbar_webdriver_ios.rb
@@ -13,18 +13,22 @@ require 'fileutils'
 
 ##
 ## IMPORTANT: Set the following parameters.
+## Make sure that the screenshot directory already exists!
 ##
 screen_shot_dir = "screenshot-folder"
-bitbar_api_key = ENV["BITBAR_APIKEY"]
-bitbar_device = "iPad 2 A1395 7.0.4" # Example device. Change if you desire.
+bitbar_api_key = "lp5pYpMvDitoGnp750y3NYNRFxrJ9uF4"
+bitbar_device = "Apple iPad 4 mini 13.2.3 /08 BS" # Example device. Change if you desire.
 bitbar_app_file = "BitbarIOSSample.ipa"
 
+##
+##  If your app is already uploaded assign its ID to the bitbar_app_id (can be found in bitbar files library)
+##
+bitbar_app_id = "47532858"
 
-def log(msg)
-  puts "#{Time.now}: #{msg}"
-end
 
-@bitbar_app = nil
+##
+##  Set other parameters if needed, see more on README
+## 
 desired_capabilities_cloud = {
     'device' => 'iphone',
     'bitbar_app' => nil,
@@ -36,11 +40,16 @@ desired_capabilities_cloud = {
     'bitbar_target' => 'ios',
     'deviceName' => 'iPhone device',
     'platformName' => 'iOS',
+    'automationName': 'XCUITest',
     'bundleId' => 'com.bitbar.testdroid.BitbarIOSSample'
 }
 
 
-server_url = 'https://appium.bitbar.com/wd/hub'
+server_url = 'https://broker-staging.bitbar.com/wd/hub'
+
+def log(msg)
+  puts "#{Time.now}: #{msg}"
+end
 
 def upload_application(file_path, bitbar_api_key)
 
@@ -53,87 +62,80 @@ def upload_application(file_path, bitbar_api_key)
   c.http_post(Curl::PostField.file("file", file_path))
   resp = JSON.parse(c.body_str)
 
-  @bitbar_app = resp["id"]
+  bitbar_app_id = resp["id"]
 end
 
 describe "BitbarIOSSample testing" do
   before :all do
-
-    FileUtils.mkdir_p 'screen_shot_dir'
-
-    log ("Upload application #{bitbar_app_file}")
-    # => upload_application(bitbar_app_file, bitbar_api_key)
-    log ("Uploaded file id #{@bitbar_app}")
-
-    desired_capabilities_cloud['bitbar_app'] = "latest"
+    if bitbar_app_id == nil
+      log ("Upload application #{bitbar_app_file}")
+      upload_application(bitbar_app_file, bitbar_api_key)
+      log ("Uploaded file id #{@bitbar_app}")
+    end
+    desired_capabilities_cloud['bitbar_app'] = bitbar_app_id
 
     log ("Start Webdriver with [#{desired_capabilities_cloud}]")
-    @appium_driver = Appium::Driver.new ({:caps => desired_capabilities_cloud, :appium_lib => {:server_url => server_url}})
-    @web_driver = @appium_driver.start_driver()
+    @driver = Appium::Driver.new ({:caps => desired_capabilities_cloud, :appium_lib => {:server_url => server_url}})
+    @web_driver = @driver.start_driver()
 
     log ("WebDriver response received")
   end
 
   after :all do
     log ("Stop WebDriver")
-
-    @appium_driver.driver_quit
+    @driver.driver_quit()
   end
 
   it "should show failure page" do
     log ("view1: Finding buttons")
-    buttons = @appium_driver.find_elements(:xpath, "//UIAApplication[1]/UIAWindow[1]/UIAButton")
-    log ("view1: Clicking button [0] - RadioButton 1")
-    buttons[0].click()
+    @driver.find_element(:name, "answer1").click
 
-    log ("view1: Typing in textfield[0]: Testdroid user")
-    @appium_driver.find_element(:name, "userName").send_keys("Testdroid user\n")
+    log ("view1: Typing in edit field: Bitbar user")
+    @driver.find_element(:name, "your name").send_keys("Bitbar user\n")
+    @driver.hide_keyboard('return', :pressKey)
+    sleep(2)
 
     log ("view1: Taking screenshot screenshot1.png")
-    @appium_driver.screenshot(screen_shot_dir + "/screenshot1.png")
-    sleep(5)
+    @driver.screenshot(screen_shot_dir + "/screenshot1.png")
+    sleep(2)
 
-    log ("view1: Taking screenshot screenshot2.png")
-    @appium_driver.screenshot(screen_shot_dir + "/screenshot2.png")
-    sleep(5)
-    log ("view1: Clicking button[6] - Answer  Button")
-    buttons[6].click()
+    log ("view1: Clicking Send Answer  Button")
+    @driver.find_element(:name, "sendAnswer").click
+    sleep(2)
 
-    log ("view2: Taking screenshot screenshot3.png")
-    @appium_driver.screenshot(screen_shot_dir + "/screenshot3.png")
-    sleep(5)
+    log ("view2: Taking screenshot screenshot2.png")
+    @driver.screenshot(screen_shot_dir + "/screenshot2.png")
+    sleep(2)
   end
 
   it "should click back button" do
     # view2
-    log ("view2: Finding buttons")
-    buttons = @appium_driver.find_elements(:xpath, "//UIAApplication[1]/UIAWindow[1]/UIAButton")
-    log ("view2: Clicking button[0] - Back/OK button")
-    buttons[0].click()
+    log ("view2: Clicking button - Back/OK button")
+    @driver.find_element(:name, "back").click
+    sleep(2)
+    log ("view2: Taking screenshot screenshot3.png")
+    @driver.screenshot(screen_shot_dir + "/screenshot3.png")
   end
 
   it "should click 2nd radio button" do
 
-    log ("view1: Clicking button[2] - RadioButton 2")
+    log ("view1: Clicking answer2 - RadioButton 2")
 
-    buttons = @appium_driver.find_elements(:xpath, "//UIAApplication[1]/UIAWindow[1]/UIAButton")
-    buttons[2].click()
-
-    log ("view1: Clicking button[6] - Answer Button")
-    buttons[6].click()
-
+    @driver.find_element(:name, "answer2").click
+    sleep(1)
     log ("view1: Taking screenshot screenshot4.png")
-    @appium_driver.screenshot(screen_shot_dir + "/screenshot4.png")
+    @driver.screenshot(screen_shot_dir + "/screenshot4.png")
+
+    log ("view1: Clicking Send Answer Button")
+    @driver.find_element(:name, "sendAnswer").click
+
+    log ("view1: Taking screenshot screenshot5.png")
+    @driver.screenshot(screen_shot_dir + "/screenshot5.png")
 
     log ("view1: Sleeping 3 before quitting webdriver")
     sleep(3)
 
   end
 
-  it "should get window size" do
-    size = @appium_driver.manage.window.size
-    size.width.should eq(768)
-    size.height.should eq(1024)
-  end
 
 end
