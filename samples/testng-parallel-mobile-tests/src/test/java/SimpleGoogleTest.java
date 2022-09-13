ae5dee39eee36f7e6297bda842c1e62c3a37df9d
MaciejLendzion-smartbear <108719957+MaciejLendzion-smartbear@users.noreply.github.com>
2022-09-13 09:22:40 +0200

    SDCB-3378-Testing-parallel-security-fix

--- samples/testng-parallel-mobile-tests/src/test/java/SimpleGoogleTest.java
+++ samples/testng-parallel-mobile-tests/src/test/java/SimpleGoogleTest.java
@@ -1,7 +1,6 @@
 import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
