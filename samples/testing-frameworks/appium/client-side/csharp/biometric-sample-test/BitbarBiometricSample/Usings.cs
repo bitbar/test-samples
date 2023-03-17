69af7e774b09ddeb5abf652d3b061223dbf6e17a
MaciejLendzion-smartbear <maciej.lendzion@smartbear.com>
2023-03-17 09:20:24 +0100

    SDCB-8011

---
+++ samples/testing-frameworks/appium/client-side/csharp/biometric-sample-test/BitbarBiometricSample/Usings.cs
@@ -0,0 +1,5 @@
+global using NUnit.Framework;
+global using OpenQA.Selenium.Remote;
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Appium;
+global using OpenQA.Selenium.Appium.Android;
