92e4a6a30397d0970bad6156e4e44e528c8e0f70
Michał Przytarski <michal.przytarski@smartbear.com>
2023-03-24 12:39:34 +0100

    Remove recreating screenshots dir

--- samples/testing-frameworks/appium/server-side/javascript/biometricInstrumentation/webdriver.io/run-tests.sh
+++ samples/testing-frameworks/appium/server-side/javascript/biometricInstrumentation/webdriver.io/run-tests.sh
@@ -5,12 +5,6 @@ appium --log-no-colors --log-timestamp >/dev/null
 
 echo "Preparing..."
 
-# Make sure there's no pre-existing `screenshots` file blocking symbolic link creation
-rm -rf screenshots
-
-# Recreate screenshots dir
-mkdir screenshots
-
 echo "Extracting tests.zip..."
 unzip tests.zip
 
