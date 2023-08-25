3d749119b62b4ec89a7b8b6307efb8d54e3f4fdc
MaciejLendzion-smartbear <108719957+MaciejLendzion-smartbear@users.noreply.github.com>
2023-08-25 12:43:43 +0200

    SDCB-9994 [Appium 2.0 Support] JavaScript CS sample (#507)

--- samples/testing-frameworks/appium/client-side/javascript/nightwatch_web_example/tests/example_test.js
+++ samples/testing-frameworks/appium/client-side/javascript/nightwatch_web_example/tests/example_test.js
@@ -3,7 +3,6 @@ module.exports = {
     'Basic Test Example' : function (browser) {
         browser
             .url('https://bitbar.github.io/web-testing-target/')
-            .waitForElementPresent('body', 4000)
             .pause(2000)
             .assert.title("Bitbar - Test Page for Samples")
             .end();
