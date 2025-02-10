f39fba231f2f1392cfe70549934726c2f14c9481
Artur Cwiklinski <artur.cwiklinski@smartbear.com>
2025-02-10 14:18:57 +0100

    Update appium/client-side/javascript/nightwatch_web_example

--- samples/testing-frameworks/appium/client-side/javascript/nightwatch_web_example/gulpfile.js
+++ samples/testing-frameworks/appium/client-side/javascript/nightwatch_web_example/gulpfile.js
@@ -1,64 +1,28 @@
-var gulp = require('gulp'),
-    nightwatch = require('gulp-nightwatch'),
-    inject = require('gulp-inject-string'),
-    zip = require('gulp-zip');
+const gulp = require('gulp'),
+  nightwatch = require('gulp-nightwatch'),
+  replace = require('gulp-replace');
 
-// Client Side Execution
+function build_client_side() {
+  const credentials = require('./.credentials.json');
+  const apiKey = credentials["apiKey"];
 
-function build_client_side(){
-  var credentials = require('./.credentials.json');
-  var apiKey = credentials["apiKey"];
-
-  var build_client_side = gulp.src('./nightwatch.json')
-  .pipe(inject.replace('<insert_your_apiKey_to_credentials_file>', apiKey))
-  .pipe(gulp.dest('./build/'));
-  return build_client_side;
+  return gulp.src('./nightwatch.json')
+    .pipe(replace('<insert_your_apiKey_to_credentials_file>', apiKey))
+    .pipe(gulp.dest('./build/'));
 }
 
-
-gulp.task('ios', function() {
+gulp.task('ios', function () {
   return build_client_side()
     .pipe(nightwatch({
       configFile: 'build/nightwatch.json',
-      cliArgs: [ '--env bitbar_ios']
+      cliArgs: ['--env bitbar_ios']
     }));
 });
 
-gulp.task('android', function() {
+gulp.task('android', function () {
   return build_client_side()
     .pipe(nightwatch({
       configFile: 'build/nightwatch.json',
-      cliArgs: [ '--env bitbar_android']
+      cliArgs: ['--env bitbar_android']
     }));
-});
-
-
-// Server Side Execution
-
-function build_server_side(){
-  var build_server_side = gulp.src('./nightwatch.json')
-    .pipe(gulp.dest('./build'));
-  return build_server_side;
-}
-
-gulp.task('androidServerSide', function() {
-  return build_server_side()
-    .pipe(nightwatch({
-      configFile: 'build/nightwatch.json',
-      cliArgs: [ '--env bitbar_android_server_side','--verbose']
-    }));
-});
-
-gulp.task('iosServerSide', function() {
-  return build_server_side()
-    .pipe(nightwatch({
-      configFile: 'build/nightwatch.json',
-      cliArgs: [ '--env bitbar_ios_server_side','--verbose']
-    }));
-});
-
-gulp.task('zip', function() {
-    return gulp.src(['**','!.credentials.json','!tests/reports/*','!build/*','!dist/*'])
-        .pipe(zip('server_side_package.zip'))
-        .pipe(gulp.dest('dist'));
 });
